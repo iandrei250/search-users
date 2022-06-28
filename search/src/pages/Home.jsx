464f4845da88b5afce5742e8ae615f01e4776aa3
iandrei250 <prtdace@gmail.com>
2022-06-28 15:16:28 +0200

    components

---
+++ search/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import UserSearch from "../components/UserSearch";
+import UserList from "../components/UserList";
+
+function Home() {
+  return (
+    <div>
+      <UserSearch />
+      <UserList />
+    </div>
+  );
+}
+
+export default Home;
